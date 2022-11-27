72163a93a91645bff5097cbac060d52cbaa7df4b
Sinant <baroona@gmail.com>
2022-11-27 17:29:02 +0000

    fixed plot func some more

--- voyagerpy/plotting/plot.py
+++ voyagerpy/plotting/plot.py
@@ -12,12 +12,12 @@ import numpy as np
 from anndata import AnnData
 
 
-from typing import Optional, Union, Mapping,Sequence, Collection, Iterable, List    
+from typing import Optional, Union, Mapping,Sequence, Collection, Iterable, List, Tuple    
 
 from matplotlib.axes import Axes
 from matplotlib import pyplot as plt
 
-
+import seaborn as sns
 
 import geopandas as gpd
 
@@ -30,51 +30,6 @@ from .. import spatial as spt
         
         
 
-
-
-    
-    
-# def plot_overlay(adata:AnnData,tissue=True,col="red",colorbar=False):
-#     if(is_highres(adata)):
-#         img_size = 2000
-#         plot_loc = "hires"
-#     else:
-#         img_size= 600
-#         plot_loc = "lowres"
-#     fig = plt.figure(figsize = (10,10))
-#     plt.imshow(adata.uns["spatial"]["img"][plot_loc])
-#     #fig = plt.figure()
-#     size = fig.get_size_inches()*fig.dpi
-#     size = size/img_size
-#     #scaling = adata.uns["spatial"]["scale"]
-#     #plt.plot(200, 300, marker="o", markersize=20, markeredgecolor="red", markerfacecolor="green")
-#     h_sc = adata.uns["spatial"]["scale"]["tissue_hires_scalef"]
-#     circ = adata.uns["spatial"]["scale"]["fiducial_diameter_fullres"]
-#     #circ = adata.uns["spatial"]["scale"]["spot_diameter_fullres"]
-    
-#     # if(col is not None):
-#     #     col = "red"
-    
-#     x,y = get_spot_coords(adata,tissue)
-    
-#     #plt.plot(x, y, marker="o", markersize=circ*h_sc*size[0], markeredgecolor=sp_col, markerfacecolor=sp_col,alpha=.5)
-#     plt.scatter(x,y,marker="o",c=col,s=circ*h_sc*size[0],alpha= 0.5)
-#     if(colorbar):
-#         plt.colorbar(orientation="horizontal",shrink=1)
-        
-    
-#     # for i in range(adata.obs.shape[0]):
-#     #     #print(adata.obs.iloc[i,3])
-        
-#     #     x = h_sc *  adata.obs.iloc[i,4]
-#     #     y = h_sc *  adata.obs.iloc[i,3]
-#     #     if(adata.obs.iloc[i,0] == 1):
-#     #         sp_col = "red"
-#     #         plt.plot(x, y, marker="o", markersize=circ*h_sc*size[0], markeredgecolor=sp_col, markerfacecolor=sp_col,alpha=.5)
-#     #     else:
-#     #         sp_col = "blue"
-        
-#     plt.show()
     
     
 def plot_spatial_features(
@@ -85,18 +40,25 @@ def plot_spatial_features(
         annot_geom = None,
         tissue=True,
         colorbar=False,
-        color = "blue",
+        color = None,
         cmap = "Blues",
-        annot_style = None,
+        geom_style : Optional[dict] = {},
+        annot_style : Optional[dict] = {},
         ax: Optional[Axes] = None,
+        subplot_kwds : Optional[dict] = {},
+        legend_kwds : Optional[dict] = {},
         **kwds,
 ):
-    
+    sns.set_theme()
     
     #check input
     if(("geometry" not in adata.obs) or "geom" not in adata.uns["spatial"]):
         adata = spt.get_geom(adata)
-    
+    for i in features:
+        if(i not in adata.obs and i not in adata.var.index):
+            raise ValueError(
+                 f"Cannot find {i!r} in adata.obs or gene names"
+             )
     #copy observation dataframe so we can edit it without changing the inputs
     obs = adata.obs
     
@@ -150,15 +112,24 @@ def plot_spatial_features(
                 ncols = 3
         else:
             nrows = ceil(plt_nr/ncols)
+            
+        # if(subplot_kwds is None):
+        #     fig, axs = plt.subplots(nrows=nrows, ncols=ncols,figsize=(10,10))
+        if("figsize" in subplot_kwds):
+            fig, axs = plt.subplots(nrows=nrows, ncols=ncols,**subplot_kwds)
+        else:
+            fig, axs = plt.subplots(nrows=nrows, ncols=ncols,figsize = (10,10),**subplot_kwds)
 
-        fig, axs = plt.subplots(nrows=nrows, ncols=ncols,figsize=(10, 10))
+            
+
         plt.subplots_adjust(wspace = 1/ncols +  0.2)
     
     #iterate over features to plot
     x = 0
     y = 0
-    for i in range(len(feat_ls)):
     
+    for i in range(len(feat_ls)):
+        legend_kwds_ = legend_kwds
     
         if(tissue==True):
 
@@ -190,13 +161,31 @@ def plot_spatial_features(
         if(ncols == 1 and nrows == 1):
             ax = axs
             
+        
+        #correct legend if feature is categorical and make sure title is in there
+        if(len(legend_kwds_) == 0 ):
             
+            if(feat_ls[i] in adata.var.index or adata.obs[feat_ls[i]].dtype != 'category'):
+                legend_kwds_ = {'label': feat_ls[i],'orientation': "vertical","shrink":0.3}
+            else:
+                legend_kwds_ = {'title': feat_ls[i]}
+        else:
+            if(feat_ls[i] in adata.var.index or adata.obs[feat_ls[i]].dtype != 'category'):
+                legend_kwds_.setdefault('label', feat_ls[i])
+                legend_kwds_.setdefault('orientation', "vertical")
+                legend_kwds_.setdefault('shrink', 0.3)
+            else:
+
+                legend_kwds_.setdefault('title', feat_ls[i])
             
-            
-            
-            
-            
-        obs.plot(feat_ls[i], ax=ax, legend=True,cmap=cmap,legend_kwds={'label': feat_ls[i],'orientation': "vertical","shrink":0.3})    
+        if(color is not None):
+            cmap = None
+        #change default behaviour in seaborn and have no color for edges of polygons
+        geom_style.setdefault("edgecolor","none")
+        
+        
+        
+        obs.plot(feat_ls[i], ax=ax,color = color,legend=True,cmap=cmap,legend_kwds=legend_kwds_,**geom_style,**kwds)    
         if(annot_geom is not None):
             if(annot_geom in adata.uns["spatial"]["geom"]):
                 
@@ -214,17 +203,27 @@ def plot_spatial_features(
                 )
                 
             pass
+        
+        
+    
+ 
         y = y + 1
         if(y >= ncols):
             y = 0
             x = x + 1
-
+    #colorbar title
+    axs = fig.get_axes()
+    for i in range(len(axs)):
+        if(axs[i].properties()["label"] == "<colorbar>"):
+            
+            axs[i].set_title(axs[i].properties()["ylabel"],ha="left")
+            axs[i].set_ylabel("")
             
     
     if(ax is None):
-        return axs
+        return axs,fig
     else:
-        return ax
+        return ax,fig
     
     
     
