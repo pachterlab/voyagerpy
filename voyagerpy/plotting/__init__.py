15e26aa822e752de5ce3cce06ebfca7b68d9850d
Petur Helgi Einarsson <peturhelgi@gmail.com>
2022-12-12 16:18:42 +0000

    Add plot_features to plotting

--- voyagerpy/plotting/__init__.py
+++ voyagerpy/plotting/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from voyagerpy.plotting.plot import plot_coldata as plot_features
 from voyagerpy.plotting.plot import plot_spatial_features
 
 __all__ = [
     "plot_spatial_features",
+    "plot_features",
 ]
