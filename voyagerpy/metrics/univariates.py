617627f101b8596b2a06836e557a07efd6623a97
Sinant <baroona@gmail.com>
2023-01-09 14:27:30 +0000

    added univariates

--- voyagerpy/metrics/univariates.py
+++ voyagerpy/metrics/univariates.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
-
+import libpysal
 # need morean for data on obs, spatial and non spatial.
 # then need moran for 
 def moran(adata, gene):
+    from esda.moran import Moran
     # get graph object
-    vg = get_visium_graph(adata)
+    wg = get_visium_graph(adata)
     # y = get gene data from adata.X.todense()
 
-    # calculate for one, for all or a subset.
-    # mi = Moran(y, w)
-
-    # return for one or many genes
+    y = get_correct_gene_form_from_genes(gene)
+    mi = Moran(y, wg)
+    mi.I
 
 
 function_map = {
@@ -53,15 +53,5 @@ def calculate_univariate(
 
     return val
 
-import libpysal
 
-w = libpysal.io.open(libpysal.examples.get_path("stl.gal")).read()
-f = libpysal.io.open(libpysal.examples.get_path("stl_hom.txt"))
-y = np.array(f.by_col["HR8893"])
-from esda.moran import Moran
 
-y
-w
-w1, w2 = w.full()
-mi = Moran(y, w)
-mi.I
