3928cf500ddd03d26f219df778eb5f4fb4ef698c
Petur Helgi Einarsson <peturhelgi@gmail.com>
2023-01-26 13:28:36 +0000

    add find_visum_graph function

--- voyagerpy/spatial/graphs.py
+++ voyagerpy/spatial/graphs.py
@@ -1,11 +1,23 @@
-from typing import Any, List, Tuple, Union
+from collections import defaultdict
+from typing import (
+    Any,
+    List,
+    Optional,
+    Tuple,
+    Union,
+)
 
 import geopandas as gpd
 import networkx as nx
 import numpy as np
+import pandas as pd
+import scipy.sparse as sp
 import shapely
 from anndata import AnnData
+from shapely.geometry import Point, Polygon
 
+from voyagerpy import utils
+from voyagerpy.spatial import spatial
 
 
 def compute_weights(coords: gpd.GeoSeries, func="euclidean") -> Any:
@@ -50,25 +62,75 @@ def dnn(
     ret = [np.where(pred(d))[0] for d in W]
     return ret
 
+
 def graph2np(g: Any) -> List[List[Tuple[str, float]]]:
     neighbour_list = []
 
     return neighbour_list
 
 
-_spatial_nb_methods = ['tri2nb', 'knn', 'dnn', 'gabriel', 'relative', 'soi', 'poly2nb']
-_dist_types = ['none', 'idw', 'exp', 'dpd']
+_spatial_nb_methods = ["tri2nb", "knn", "dnn", "gabriel", "relative", "soi", "poly2nb"]
+_dist_types = ["none", "idw", "exp", "dpd"]
 
 
-def findVisiumGraph(adata: AnnData) -> Any:
+def compute_visium_dists(adata, filt):
+    select = select_obs(adata, filt)
 
-    centroids = gpd.GeoSeries(adata.obs[adata.obs['in_tissue'] == 1]['spot_poly']).centroid
+    coords = adata[select].obs[["array_col", "array_row"]] * np.array([1, np.sqrt(3)])
+    positions = gpd.GeoSeries(map(Point, coords.values), index=coords.index)
+    dists = compute_weights(positions)
+    return dists
+
+
+def select_obs(adata: AnnData, filt: Optional[Tuple[str, Any]]) -> Union[slice, pd.Series]:
+    if filt is None:
+        return slice(None)
+
+    filt_key, filt_val = filt
+    if not isinstance(filt_val, (tuple, list)):
+        filt_val = (filt_val,)
+
+    return adata.obs[filt_key].isin(filt_val)
+
+
+def compute_visium_graph(adata, filt, dist_key: str) -> sp.csr_matrix:
+    if dist_key not in adata.obsp:
+        select = select_obs(adata, filt)
+        dists = compute_visium_dists(adata, filt)
+
+        if filt is not None:
+            (idx,) = np.where(select)
+            rows = np.tile(idx, len(idx))
+            cols = np.repeat(idx, len(idx))
+            matrix_data = (dists.values.ravel(), (rows, cols))
+
+            adata.obsp[dist_key] = sp.csr_matrix(matrix_data, shape=(adata.n_obs, adata.n_obs))
+        else:
+            adata.obsp[dist_key] = sp.csr_matrix(dists.values, shape=(adata.n_obs, adata.n_obs))
+
+    visium_graph = dnn(adata.obsp[dist_key].todense(), max_dist=3, full=True).astype(float)
+    return sp.csr_matrix(visium_graph, shape=(adata.n_obs, adata.n_obs))
+
+
+def find_visium_graph(
+    adata: AnnData, filt=("in_tissue", 1), graph_key: str = "visium", dist_key: str = "dists", row_normalize: bool = True
+) -> nx.Graph:
+    if graph_key not in adata.obsp:
+        adata.obsp[graph_key] = compute_visium_graph(adata, filt, dist_key)
+        # adata.obsp[graph_key].eliminate_zeros()
+
+    if row_normalize is True:
+        adata.obsp[graph_key] = utils.normalize_csr(adata.obsp[graph_key], byrow=True)
+
     G = nx.Graph()
-    dists = compute_weights(centroids)
-    positions = list(zip(centroids.x, centroids.y))
-    # TODO: max_dist by scale
-    edge_mat = dnn(dists, max_dist=30, full=True)
-    edges = list(zip(*np.where(edge_mat)))
-    G.add_edges_from(edges)
+    nodes_out, nodes_in = np.where(adata.obsp[graph_key].todense())
 
-    g = dnn(coords)
+    # Add node names or mappings
+    edges = list(zip(nodes_out, nodes_in))
+    G.add_edges_from(edges)
+    nx.relabel_nodes(G, dict(enumerate(adata.obs.index)), copy=False)
+
+    positions: pd.DataFrame = spatial.get_spot_coords(adata, tissue=True, as_df=True)
+    positions = dict(zip(positions.index, positions.values))
+    nx.set_node_attributes(G, positions, "pos")
+    return G
