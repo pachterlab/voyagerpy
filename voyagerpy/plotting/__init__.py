bef1e13f6f1bade11969565a7e75043111cd175e
Petur Helgi Einarsson <peturhelgi@gmail.com>
2023-02-06 15:44:35 +0000

    cleanup plotting/__init__.py

--- voyagerpy/plotting/__init__.py
+++ voyagerpy/plotting/__init__.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import matplotlib
-from matplotlib import colormaps
-from matplotlib.colors import ListedColormap
-from matplotlib.pyplot import rcParams, rc_context
+from matplotlib.pyplot import rc_context, rcParams
 
 from voyagerpy.plotting.ditto_colors import register_dittoseq
 from voyagerpy.plotting.plot import (
     elbow_plot,
+    grouped_violinplot,
+    imshow,
     plot_barcode_data,
     plot_barcodes_bin2d,
     plot_bin2d,
     plot_dim_loadings,
+    plot_expression,
     plot_features_bin2d,
     plot_pca,
     plot_spatial_feature,
+    simple_violinplot,
     spatial_reduced_dim,
 )
 
@@ -24,23 +25,21 @@ def set_default_cmap(cmap_name: str) -> None:
     rcParams["image.cmap"] = cmap_name
 
 
-def with_colormap(cmap_name: str):
-    return rc_context({"image.cmap": cmap_name})
-
-
-with_cmap = with_colormap
-
 set_default_cmap(register_dittoseq())
 
 
 __all__ = [
     "elbow_plot",
-    "plot_barcodes_bin2d",
+    "grouped_violinplot",
+    "imshow",
     "plot_barcode_data",
+    "plot_barcodes_bin2d",
     "plot_bin2d",
     "plot_dim_loadings",
+    "plot_expression",
     "plot_features_bin2d",
     "plot_pca",
     "plot_spatial_feature",
+    "simple_violinplot",
     "spatial_reduced_dim",
 ]
