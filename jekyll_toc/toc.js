65df4ec050152a5e0d6e3309cdf9be76b585c25c
Petur Helgi Einarsson <peturhelgi@gmail.com>
2023-02-15 10:33:27 +0000

    Add correct bootstrap-icon class to the toplinks in toc.js

--- jekyll_toc/toc.js
+++ jekyll_toc/toc.js
@@ -54,7 +54,7 @@
 
     var get_level = function (ele) { return parseInt(ele.nodeName.replace("H", ""), 10); };
     var highest_level = headers.map(function (_, ele) { return get_level(ele); }).get().sort()[0];
-    var return_to_top = '<i class="icon-arrow-up back-to-top"> </i>';
+    var return_to_top = ' <i class="bi bi-arrow-up back-to-top"> </i>';
 
     var level = get_level(headers[0]),
       this_level,
