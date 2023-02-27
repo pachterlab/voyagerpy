27745536449d01d768ba0190fd72fb366cf2f97d
Petur Helgi Einarsson <peturhelgi@gmail.com>
2023-02-27 15:03:45 +0000

    Add a DivergentNorm normalization class for normalizing diverging colormaps

--- voyagerpy/plotting/cmap_helper.py
+++ voyagerpy/plotting/cmap_helper.py
@@ -1,7 +1,8 @@
-from matplotlib import colormaps
-from matplotlib.colors import LinearSegmentedColormap, ListedColormap
-import pathlib
 from typing import Tuple
+
+import numpy as np
+from matplotlib import colormaps
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap, Normalize
 
 
 def register_segmented_cmap(cmap_name: str, cmap_colors: Tuple[str], reverse=False) -> None:
@@ -26,3 +27,26 @@ def register_listed_cmap(cmap_name: str, cmap_colors: Tuple[str], reverse: bool 
     dittoseq_cmap = ListedColormap(cmap_colors, name=cmap_name, N=len(cmap_colors))
     colormaps.register(dittoseq_cmap)
     colormaps.register(dittoseq_cmap.reversed())
+
+
+class DivergentNorm(Normalize):
+    def __init__(self, vmin, vmax, vcenter=0, clip=False):
+        Normalize.__init__(self, vmin, vmax, clip)
+
+        # The largest interval defines the slope
+        dx = max(abs(vmax - vcenter), abs(vcenter - vmin))
+
+        # The y difference of the dx is always 0.5
+        dy = 0.5
+        slope = dy / dx
+        intercept = 0.5 - slope * vcenter
+
+        f = lambda x: slope * x + intercept
+        self.x = [self.vmin, self.vmax]
+        self.y = [f(self.vmin), f(self.vmax)]
+
+    def __call__(self, value, clip=None):
+        return np.ma.masked_array(np.interp(value, self.x, self.y))
+
+    def inverse(self, value):
+        return np.ma.masked_array(np.interp(value, [0, 1], self.x))
