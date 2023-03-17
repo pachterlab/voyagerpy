4f99f9a68a8ad1fd67ecd20ced5315405cc6cf82
Petur Helgi Einarsson <peturhelgi@gmail.com>
2023-03-17 15:42:13 +0000

    add the spatial autocorrelation functions to the package

--- voyagerpy/spatial/__init__.py
+++ voyagerpy/spatial/__init__.py
@@ -14,11 +14,18 @@ from voyagerpy.spatial.spatial import (
     mirror_img,
     rollback_transforms,
     rotate_img90,
+    to_spatial_weights,
+    moran,
+    compute_spatial_lag,
+    local_moran,
+    set_geometry,
+    to_points,
 )
 
 __all__ = [
     "apply_transforms",
     "cancel_transforms",
+    "compute_spatial_lag",
     "detect_tissue_threshold",
     "get_approx_tissue_boundary",
     "get_geom",
@@ -28,4 +35,9 @@ __all__ = [
     "mirror_img",
     "rollback_transforms",
     "rotate_img90",
+    "to_spatial_weights",
+    "moran",
+    "local_moran",
+    "set_geometry",
+    "to_points",
 ]
