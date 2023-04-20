3568d891671a462d041d983574f5370e9a2db40f
Petur Helgi Einarsson <peturhelgi@gmail.com>
2023-04-20 13:45:57 +0000

    Add the Blues(3..9) colormaps from R ColorBrewer

---
+++ voyagerpy/plotting/blues_colors.py
@@ -0,0 +1,8 @@
+
+blues3 = ("#DEEBF7", "#9ECAE1", "#3182BD")
+blues4 = ("#EFF3FF", "#BDD7E7", "#6BAED6", "#2171B5")
+blues5 = ("#EFF3FF", "#BDD7E7", "#6BAED6", "#3182BD", "#08519C",)
+blues6 = ("#EFF3FF", "#C6DBEF", "#9ECAE1", "#6BAED6", "#3182BD", "#08519C",)
+blues7 = ("#EFF3FF", "#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#084594",)
+blues8 = ("#F7FBFF", "#DEEBF7", "#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#084594")
+blues9 = ("#F7FBFF", "#DEEBF7", "#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B")
