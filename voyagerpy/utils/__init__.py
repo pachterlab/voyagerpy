58e6bb6824cd0a3eb6cff7ea23c2741d1a9cbde5
Sinant <baroona@gmail.com>
2023-04-24 14:46:56 +0000

    added hvgs prelim

--- voyagerpy/utils/__init__.py
+++ voyagerpy/utils/__init__.py
@@ -14,15 +14,19 @@ from voyagerpy.utils.utils import (
     scale,
 )
 
+from voyagerpy.utils.hvg import get_top_hvgs, model_gene_var
+
 __all__ = [
     "add_per_cell_qcmetrics",
     "add_per_gene_qcmetrics",
+    "get_top_hvgs",
     "get_scale",
     "is_highres",
     "kurtosis",
     "listify",
     "log_norm_counts",
     "make_unique",
+    "model_gene_var",
     "normalize_csr",
     "scale",
 ]
