9ef7386ee1902fa6837140acfec1a802712e95a4
Sinant <baroona@gmail.com>
2023-04-26 17:38:18 +0000

    exporting new funcs

--- voyagerpy/utils/__init__.py
+++ voyagerpy/utils/__init__.py
@@ -15,15 +15,17 @@ from voyagerpy.utils.utils import (
 )
 
 from voyagerpy.utils.hvg import get_top_hvgs, model_gene_var
-from voyagerpy.utils.markers import get_marker_genes, get_p_clusters
+from voyagerpy.utils.markers import get_marker_genes, get_p_clusters, find_markers, get_stat_clusters
 
 __all__ = [
     "add_per_cell_qcmetrics",
     "add_per_gene_qcmetrics",
+    "find_markers",
     "get_marker_genes",
     "get_p_clusters",
     "get_top_hvgs",
     "get_scale",
+    "get_stat_clusters",
     "is_highres",
     "kurtosis",
     "listify",
