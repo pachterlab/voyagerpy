4fc4d8988831a44e426735e6f0679e6ae814150d
Petur Helgi Einarsson <peturhelgi@gmail.com>
2023-07-18 16:22:16 +0000

    Add plt (plotting), spt (spatial), and utl (utils) to the voyagerpy package

--- voyagerpy/__init__.py
+++ voyagerpy/__init__.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from voyagerpy import plotting, spatial, utils
+from voyagerpy.read import read_10x_counts, read_10x_visium
+
 __version__ = "0.1.1"
 
-
-from voyagerpy import metrics as mtr
-from voyagerpy import plotting as plt
-from voyagerpy import spatial as spt
-from voyagerpy import utils as utl
-from voyagerpy.read import read_10x_visium
-from voyagerpy.read import read_10x_counts
-
+plt = plotting
+spt = spatial
+utl = utils
 
 __all__ = [
-    "metrics",
     "plotting",
-    "spatial",
-    "utils",
+    "plt",
     "read_10x_counts",
     "read_10x_visium",
+    "spatial",
+    "spt",
+    "utils",
+    "utl",
 ]
